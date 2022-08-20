6dfe40b61503ba810cc4bc2fb7271290063dca0e
Vladimir Kharchenko <109531719+Vladimir-kh22@users.noreply.github.com>
2022-08-20 23:05:03 +0300

    main page offers fixed

--- second_site_practice/js/script.js
+++ second_site_practice/js/script.js
@@ -1,10 +1,12 @@
 
 function burger() {
     const buttomMenu = document.querySelector('.menu__icon');
+    const bodyOverflowHidden = document.querySelector('body')
     const menuHeader = document.querySelector('.header__menu');
 
     buttomMenu.addEventListener('click', function () {
         menuHeader.classList.toggle('none');
+        bodyOverflowHidden.classList.toggle('none');
     })
 }
 burger();
